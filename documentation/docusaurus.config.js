6673f81ad9d226691956b642247856c6a7a9556b
Manuel Arias Alcocer <141689278+ManuelAriasAlcocer@users.noreply.github.com>
2025-04-30 16:36:37 -0600

    Update docusaurus.config.js

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'http://iocusdoc.sslip.io',
+  url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Iocus_documentation',
